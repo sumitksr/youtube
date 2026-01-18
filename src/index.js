bb49a629b3ab80bdba650aec19d8c4d838795089
sumitksr <sumitksr6223@gmail.com>
2026-01-18 14:45:47 +0530

    feat: update database connection logic and add async error handling middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import mongoose from "mongoose";
-import { dbconnect } from './config/database.js';
-import express from "express";
-import { PORT } from './constants.js';
-import cors from "cors";
+const mongoose = require('mongoose');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const { DB_NAME } = "../constants.js";
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const { dbconnect } = require('./config/database.js');
 const app = express();
 
 
+//CORS configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true,
+}))
+
+
+//MIDDLEWARES
+app.use(express.json({limit: '50kb'}));
+app.use(express.urlencoded({ extended: true, limit: '50kb' }));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+
+//DB CONNECTION
+dbconnect().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Failed to connect to the database:', err);
+});
+
+
+//ROUTES
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,5 @@
+exports.asyncHandler = (fn) => {
+    return function (req, res, next) {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};   
