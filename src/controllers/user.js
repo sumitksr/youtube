631538c02caad6ff8cd295d8a11c1c8d7b18b868
sumitksr <sumitksr6223@gmail.com>
2026-01-19 19:20:06 +0530

    feat: implement user registration with validation, image upload, and error handling

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,55 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { User } from '../models/user.js';
+import bcrypt from 'bcryptjs';
+require('dotenv').config();
+import {ApiError} from '../utils/ApiError.js';
+import { uploadOnCloudinary} from '../config/cloudinary.js';
 
 export const registerUser = asyncHandler(async (req, res) => {
-    res.status(201).json({message: 'User registered successfully'});
+    const { username, email, password ,fullName  } = req.body;
+     if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+        throw new ApiError(409, "User with this email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.profilePicture[0]?.path;
+    const coverPhotoLocalPath = req.files?.coverPhoto[0]?.path;
+
+    if(!avatarLocalPath ){
+        throw new ApiError(400, "Profile picture is required");
+    }
+
+    const avatarUploadResponse = await uploadOnCloudinary(avatarLocalPath);
+    const coverPhotoUploadResponse = await uploadOnCloudinary(coverPhotoLocalPath);
+
+    if (!avatarUploadResponse ) {
+        throw new ApiError(500, "Error uploading images to Cloudinary");
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+        fullName,
+        profilePicture: avatarUploadResponse.url,
+        coverPhoto: coverPhotoUploadResponse.url || ""
+    });
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+
 });
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.js';
+import {upload} from '../middlewares/multer.js';
 
 
 const router = Router();
-router.post('/register', registerUser);
-
+router.post('/register', upload.fields([
+    { name: 'profilePicture', maxCount: 1 },
+    { name: 'coverPhoto', maxCount: 1 }
+]), registerUser);
 
 export default router;
